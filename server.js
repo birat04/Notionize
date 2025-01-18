2b154d83f9deeabcf7df468ca7890e5a95b01ad9
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-01-18 11:50:52 +0530

    update the server

--- server.js
+++ server.js
@@ -35,9 +35,15 @@ app.post("/signin", async function (req, res) {
 
     const user = await UserModel.findOne({
         email: email,
-        password : password
-    });
-    const passwordMatch = bcrypt.compare(password,user.password);
+      });
+      if(!user){
+        res.status(404).json({
+          message: "User does not exist in our database"
+        })
+        return
+      }
+        
+    const passwordMatch = await bcrypt.compare(password,user.password);
     if(user && passwordMatch){
         const token = jwt.sign({
             id: user._id.toString()
