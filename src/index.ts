319b73c4ec9f0fcc11380ced4dcbc466d46c33f1
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-06-25 00:45:05 +0530

    update pg

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import {Client} from "pg";
 
-const pgClient = new Client({
-    user:"neondb_owner",
-    password:"npg_UA41EGeORmPT",
-    port: 5432,
-    host:"ep-blue-silence-a1bhsl39-pooler.ap-southeast-1.aws.neon.tech",
-    database:"neondb"
-})
+const pgClient = new Client("postgresql://neondb_owner:npg_UA41EGeORmPT@ep-blue-silence-a1bhsl39-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
 async function main() {
  await pgClient.connect()
+ const response = await pgClient.query("SELECT * FROM todo.tasks")
+    console.log(response.rows)
 }
+main()
