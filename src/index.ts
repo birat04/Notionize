c5150fd43126f9f243d7f391f5afd6d81634281e
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-06-26 22:16:45 +0530

    update pg

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+import express from "express";
 import {Client} from "pg";
 
+const app = express();
+app.use(express.json());
+
 const pgClient = new Client("postgresql://neondb_owner:npg_UA41EGeORmPT@ep-blue-silence-a1bhsl39-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
-async function main() {
- await pgClient.connect()
- const response = await pgClient.query("SELECT * FROM todo.tasks")
-    console.log(response.rows)
-}
-main()
+pgClient.connect()
+
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    const response = await pgClient.query(`
+        INSERT INTO todo.users (username, password, email) VALUES (${username}
+        , ${password}, ${email});`);
+
+        res.json({
+            message: "User created successfully",
+            user: {
+                username: username,
+                email: email
+            }
+        })
+    
+})
+app.listen(3000)
+
